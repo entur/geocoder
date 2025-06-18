f90ee63a3230c86af77576782e550f1f64cb99c8
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-06-18 12:18:16 +0200

    upgrade kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    kotlin("jvm") version "2.1.20" apply false
+    kotlin("jvm") version "2.1.21" apply false
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0-rc.1" apply false
 }
