622d90be60b1a53628edc68cb93ec32d13c5c3d7
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-10-30 14:15:48 +0100

    try photon with native metrics, pt3
    Ref https://github.com/entur/geocoder-archive/issues/48

--- photon/photon-start.sh
+++ photon/photon-start.sh
@@ -2,4 +2,4 @@
 
 find photon_data -name '*.lock' -delete
 
-java -jar photon.jar -default-language no -listen-port ${SERVER_PORT:-2322}
+java -jar photon.jar -default-language no -listen-port ${SERVER_PORT:-2322} -metrics-enable
