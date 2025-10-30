453cc3d97e7e478632dc2851d64f6d54e17ddba1
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-10-30 09:58:11 +0100

    jmx export conf for proxy too

--- proxy/proxy-start.sh
+++ proxy/proxy-start.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env sh
 
-cat > jmx-exporter.yaml <<YAML
-startDelaySeconds: 0
-ssl: false
-lowercaseOutputName: true
-lowercaseOutputLabelNames: true
-rules:
-  - pattern: ".*"
-YAML
-
-export JAVA_TOOL_OPTIONS="-javaagent:./jmx_prometheus_javaagent-1.0.1.jar=9404:./jmx-exporter.yaml"
+export JAVA_TOOL_OPTIONS="-javaagent:./jmx_prometheus_javaagent-1.0.1.jar=9404:./jmx-exporter.yml"
 java -jar proxy.jar
