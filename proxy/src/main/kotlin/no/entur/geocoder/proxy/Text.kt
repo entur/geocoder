cabe9cd527173a2b8afbb9242d622339f625ca0e
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-11-11 09:43:25 +0100

    trim categories
    see https://github.com/entur/geocoder-archive/issues/71

--- proxy/src/main/kotlin/no/entur/geocoder/proxy/Text.kt
+++ proxy/src/main/kotlin/no/entur/geocoder/proxy/Text.kt
@@ -1,7 +1,7 @@
 package no.entur.geocoder.proxy
 
 object Text {
-    fun String?.safeVar(): String? = this?.replace("[^\\p{L}\\p{Nd}_.,'\\-\\s:]+".toRegex(), " ")
+    fun String?.safeVar(): String? = this?.replace("[^\\p{L}\\p{Nd}_.,'\\-\\s:]+".toRegex(), " ")?.trim()
 
     fun List<String>?.safeVars(): List<String>? = this?.mapNotNull { it.safeVar() }
 }
