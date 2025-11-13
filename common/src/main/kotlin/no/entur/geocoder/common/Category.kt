680ea8e833688fbfcfc9404effa09f80ad075571
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-11-13 10:28:57 +0100

    Some comments regarding category

--- common/src/main/kotlin/no/entur/geocoder/common/Category.kt
+++ common/src/main/kotlin/no/entur/geocoder/common/Category.kt
@@ -1,8 +1,8 @@
 package no.entur.geocoder.common
 
 object Category {
-    const val OSM_ADDRESS = "osm.public_transport.address"
-    const val OSM_STREET = "osm.public_transport.street"
+    const val OSM_ADDRESS = "osm.public_transport.address" // Addresses with house numbers
+    const val OSM_STREET = "osm.public_transport.street" // Streets without house numbers
     const val OSM_STOP_PLACE = "osm.public_transport.stop_place"
     const val OSM_POI = "osm.public_transport.poi"
     const val OSM_GOSP = "osm.public_transport.group_of_stop_places"
