8fa284ba9b243a00c031a0580eee104409dea83f
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-11-23 13:57:42 +0100

    debug generate-tag

--- .github/actions/generate-image-tag/generate-tag.sh
+++ .github/actions/generate-image-tag/generate-tag.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 SHA=${1:0:7}
 REF=${2:-detached}
 
-branch=$(echo "$REF" | sed 's/[^a-zA-Z0-9_-]\+/_/g')
+branch=$(echo "$REF" | sed -E 's/[^a-zA-Z0-9_-]+/_/g')
 echo "${branch}.$(date +'%Y%m%d')-SHA${SHA}"
