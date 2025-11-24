c15a05dafb11a5e833cc529c63be371a73df8a26
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-11-24 09:34:36 +0100

    fix script
    [ci skip]

--- converter/download-latest-nominatim-data.sh
+++ converter/download-latest-nominatim-data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 SCRIPTDIR=$(cd $(dirname $0); pwd)
-$SCRIPTDIR/../.github/actions/download-docker-artifact/extract.sh eu.gcr.io/entur-system-1287 .
+$SCRIPTDIR/../.github/actions/download-docker-artifact/extract.sh eu.gcr.io/entur-system-1287 geocoder-nominatim-data:latest .
--- photon/photon-start.sh
+++ photon/photon-start.sh
@@ -2,5 +2,5 @@
 
 find photon_data -name '*.lock' -delete
 
-export PHOTON_VERSION=$(cat version)
+export PHOTON_VERSION="$(cat version)"
 java -jar photon.jar -default-language no -listen-port ${SERVER_PORT:-2322} -metrics-enable prometheus
