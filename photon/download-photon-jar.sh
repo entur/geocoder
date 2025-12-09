a732a14620125e95696037724eaf7179d51573cc
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-12-09 13:24:17 +0100

    temporary use photon that supports both suggest_addresses and include_housenumbers

--- photon/download-photon-jar.sh
+++ photon/download-photon-jar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-PHOTON_JAR=https://github.com/entur/photon/releases/download/housenumbers/photon-0.7.0.jar
+PHOTON_JAR=https://github.com/entur/photon/releases/download/housenumbers-temp-patch/photon-0.7.0.jar
 
 curl -sfL --retry 2 -o photon.jar $PHOTON_JAR && echo photon.jar downloaded successfully && exit 0
 
