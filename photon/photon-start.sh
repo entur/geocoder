b5ff8206030d1e297a2941abf79a2c78f62700af
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-01-12 15:46:46 +0100

    Latest photon restricts bind ip to 127.0.0.1

--- photon/photon-start.sh
+++ photon/photon-start.sh
@@ -2,4 +2,4 @@
 
 find photon_data -name '*.lock' -delete
 
-java -jar photon.jar -default-language no -listen-port ${SERVER_PORT:-2322} -metrics-enable prometheus
+java -jar photon.jar -default-language no -listen-ip 0.0.0.0 -listen-port ${SERVER_PORT:-2322} -metrics-enable prometheus
