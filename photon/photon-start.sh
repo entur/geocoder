f408fdb524f59678d34017f8c546c5fb66752288
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-01-26 09:36:38 +0100

    More memory tuning

--- photon/photon-start.sh
+++ photon/photon-start.sh
@@ -3,7 +3,7 @@
 find photon_data -name '*.lock' -delete
 
 # Align memory with helm/geocoder-photon/values.yaml. Helm values should be at least 25% higher.
-java -Xms1g -Xmx1g -XX:+UseZGC -XX:+UseStringDeduplication \
+java -Xms1536m -Xmx1536m -XX:+UseStringDeduplication -XX:MaxGCPauseMillis=100 -XX:+ExitOnOutOfMemoryError \
      -Dspark.threadPool.maxThreads=20 -jar photon.jar -default-language no \
      -listen-ip 0.0.0.0 -listen-port "${SERVER_PORT:-2322}" \
      -metrics-enable prometheus
--- proxy/proxy-start.sh
+++ proxy/proxy-start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 # Align memory with helm/geocoder-proxy/values.yaml.  Helm values should be at least 25% higher.
-java -Xms384m -Xmx384m --enable-native-access=ALL-UNNAMED -jar proxy.jar
+java -Xms640m -Xmx640m -XX:+ExitOnOutOfMemoryError -XX:MaxGCPauseMillis=100 --enable-native-access=ALL-UNNAMED \
+     -jar proxy.jar
