9c62d9a4ceb532dff167f95c598d98482eecdc96
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-01-26 09:40:29 +0100

    remove unnecessary jvm arg

--- photon/photon-start.sh
+++ photon/photon-start.sh
@@ -4,6 +4,6 @@ find photon_data -name '*.lock' -delete
 
 # Align memory with helm/geocoder-photon/values.yaml. Helm values should be at least 25% higher.
 java -Xms1536m -Xmx1536m -XX:+UseStringDeduplication -XX:MaxGCPauseMillis=100 -XX:+ExitOnOutOfMemoryError \
-     -Dspark.threadPool.maxThreads=20 -jar photon.jar -default-language no \
+     -jar photon.jar -default-language no \
      -listen-ip 0.0.0.0 -listen-port "${SERVER_PORT:-2322}" \
      -metrics-enable prometheus
