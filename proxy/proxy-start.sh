84cab23b48d28c6f4fcec56b59527f43a1c84cdd
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-01-26 12:19:03 +0100

    latency tweaks: more cpu for photon

--- proxy/proxy-start.sh
+++ proxy/proxy-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-# Align memory with helm/geocoder-proxy/values.yaml.  Helm values should be at least 25% higher.
-java -Xms640m -Xmx640m -XX:+ExitOnOutOfMemoryError -XX:MaxGCPauseMillis=100 --enable-native-access=ALL-UNNAMED \
+# Align memory with helm/geocoder-proxy/values.yaml.  Helm values should be at least 50% higher.
+java -Xms640m -Xmx640m XX:+UseG1GC -XX:+ExitOnOutOfMemoryError -XX:MaxGCPauseMillis=75 --enable-native-access=ALL-UNNAMED \
      -jar proxy.jar
