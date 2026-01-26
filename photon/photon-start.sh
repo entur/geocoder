b542d979e2dff53d29081917d226bb1bd4c0d96f
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-01-26 12:38:06 +0100

    typo

--- photon/photon-start.sh
+++ photon/photon-start.sh
@@ -3,7 +3,7 @@
 find photon_data -name '*.lock' -delete
 
 # Align memory with helm/geocoder-photon/values.yaml. Helm values should be at least 50% higher.
-java -Xms1536m -Xmx1536m XX:+UseG1GC -XX:+UseStringDeduplication -XX:MaxGCPauseMillis=75 -XX:+ExitOnOutOfMemoryError \
+java -Xms1536m -Xmx1536m -XX:+UseG1GC -XX:+UseStringDeduplication -XX:MaxGCPauseMillis=75 -XX:+ExitOnOutOfMemoryError \
      -jar photon.jar -default-language no \
      -listen-ip 0.0.0.0 -listen-port "${SERVER_PORT:-2322}" \
      -metrics-enable prometheus
