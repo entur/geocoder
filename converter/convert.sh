ad1b897a2ffcd42fb4be3e239bddbc5c22cce34f
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-02-03 11:53:59 +0100

    convert.sh should build jar every time

--- converter/convert.sh
+++ converter/convert.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env sh
 
-SCRIPTDIR=$(cd $(dirname $0); pwd)
+set -eu
+
+SCRIPTDIR=$(cd "$(dirname "$0")"; pwd)
+"$SCRIPTDIR/../gradlew" shadowJar >/dev/null
 JAR=$SCRIPTDIR/build/libs/converter-all.jar
-if [ ! -f "$JAR" ]; then
-  echo "Could not find $JAR. Run './gradlew assemble' to create it."
-  exit 1
-fi
 
-java -Xmx6g -jar $JAR $@
-
+java -Xmx6g -jar "$JAR" "$@"
