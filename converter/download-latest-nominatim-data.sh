d7d63288c68442081708e714ab2787f4c10d5bb2
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-02-04 13:37:22 +0100

    simplify suffices

--- converter/download-latest-nominatim-data.sh
+++ converter/download-latest-nominatim-data.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+SUFFIX=${1:-} # e.g. '-se'
+TAG=${2:-latest-prod}
+
 SCRIPTDIR=$(cd "$(dirname $0)"; pwd)
-$SCRIPTDIR/../.github/actions/download-docker-artifact/extract.sh eu.gcr.io/entur-system-1287 geocoder-nominatim-data:latest-prod .
+$SCRIPTDIR/../.github/actions/download-docker-artifact/extract.sh eu.gcr.io/entur-system-1287 "geocoder-nominatim-data$SUFFIX:$TAG" .
