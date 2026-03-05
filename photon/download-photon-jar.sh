c6adb63a329b825aa62be9fd7e6436caa619bd07
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-03-05 16:04:18 +0100

    Use latest Photon with Javalin 7
    https://github.com/komoot/photon/pull/1029

--- photon/download-photon-jar.sh
+++ photon/download-photon-jar.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-PHOTON_JAR=https://github.com/komoot/photon/releases/download/1.0.0/photon-1.0.0.jar
+PHOTON_JAR=https://github.com/entur/photon/releases/download/2026-03-05/photon-1.0.0-ca47acd.jar
 
 curl -sfL --retry 2 -o photon.jar $PHOTON_JAR && echo photon.jar downloaded successfully && exit 0
 
