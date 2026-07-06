68058820d0949c8767b0663a4673cbdef07b5c63
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2026-07-06 15:04:17 +0200

    slightly more verbose photon downloader
    [ci skip]

--- photon/import/download-photon-jar.sh
+++ photon/import/download-photon-jar.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 PHOTON_JAR=https://github.com/komoot/photon/releases/download/1.2.1/photon-1.2.1.jar
 
-curl -sfL --retry 2 -A "entur-geocoder" -o photon.jar $PHOTON_JAR && echo photon.jar downloaded successfully && exit 0
+curl -sfL --retry 2 -A "entur-geocoder" -o photon.jar $PHOTON_JAR && echo photon.jar downloaded successfully from $PHOTON_JAR && exit 0
 
-echo ERROR: failed downloading photon.jar && exit 1
+echo ERROR: failed downloading photon.jar from $PHOTON_JAR && exit 1
